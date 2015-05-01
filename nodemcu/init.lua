a07c7b2df5da1c6655ea6ea05dcd1566cd611831
Vee Satayamas <vsatayamas@gmail.com>
2015-05-01 14:16:24 +0700

    handle dht11

--- nodemcu/init.lua
+++ nodemcu/init.lua
@@ -1,23 +1,34 @@
+DHT = require("dht_lib")
+
 wifi.setmode(wifi.STATION)
 wifi.sta.config("ssid","password")
 
 gpio.mode(2,gpio.OUTPUT)
-conn=net.createConnection(net.TCP, false) 
 
-conn:on("receive",
-		function(conn, pl)
-		   if pl == 'on' then
-			  gpio.write(2, 1)
-		   else
-			  gpio.write(2, 0)
+receiver = function(conn, pl)
+   if pl == 'on' then
+	  gpio.write(2, 0)
+   else
+	  gpio.write(2, 1)
+   end
+   conn:close()			  
+end
+
+sender = function()
+   DHT.read11(4)
+
+   local h = DHT.getHumidity()
+   local t = DHT.getTemperature()
+   local v = "V," .. tostring(h) .. "," .. tostring(t)
+   
+   conn = net.createConnection(net.TCP, false)
+   conn:connect(6981, 'host address')
+   conn:on("receive", receiver)
+   conn:on("connection",
+		   function()
+			  conn:send(v)
 		   end
-		end
-)
+   )
+end
 
-conn:connect(6981, 'server')
-
-tmr.alarm(1,1000,1,
-		  function()
-			 conn:send('titi')
-		  end
-)
+tmr.alarm(1,2000,1, sender)
